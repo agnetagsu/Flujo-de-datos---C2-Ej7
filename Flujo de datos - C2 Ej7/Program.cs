6b9dbe0dd3561ec6ddec5abde98a4a5e76283369
agnetagsu <84595453+agnetagsu@users.noreply.github.com>
2021-05-22 20:34:07 +0200

    Update Program.cs

--- Flujo de datos - C2 Ej7/Program.cs
+++ Flujo de datos - C2 Ej7/Program.cs
@@ -9,7 +9,7 @@ namespace Flujo_de_datos___C2_Ej7
 
             for(int i = 1; i <= 100; i++)
             {
-                Console.WriteLine("{0}\n",i);
+                Console.WriteLine(i);
             }
 
         }
