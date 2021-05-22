33b9b3e411bd599f7b7c8e5cac15ee07fd3840b7
Agneta <agnetagsu@yahoo.es>
2021-05-22 20:30:08 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Flujo_de_datos___C2_Ej7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("{0}\n",i);
+            }
+
+        }
+    }
+}
